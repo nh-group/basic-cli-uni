701f9b0636ea7a18a6571cfaaab3e0ad4985fff1
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-03-04 08:04:31 +0100

    added logger

--- src/main/java/fr/pantheonsorbonne/ufr27/app/App.java
+++ src/main/java/fr/pantheonsorbonne/ufr27/app/App.java
@@ -1,21 +1,20 @@
 package fr.pantheonsorbonne.ufr27.app;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import fr.pantheonsorbonne.ufr27.Secured;
 import fr.pantheonsorbonne.ufr27.action.EducationAction;
 import fr.pantheonsorbonne.ufr27.configuration.AppConfiguration;
 import fr.pantheonsorbonne.ufr27.console.ConsoleEventParser;
 import fr.pantheonsorbonne.ufr27.exception.EducationException;
-
 
 /**
  * Hello world!
@@ -23,14 +22,18 @@ import fr.pantheonsorbonne.ufr27.exception.EducationException;
  */
 public class App {
 
-	private static final Logger LOG = Logger.getLogger("App");
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
+
+		LOG.info(new App().toString());
+		Arrays.stream(App.class.getDeclaredFields()).forEach((Field f) -> LOG.info("field in App: " + f.getName()));
 
 		Injector injector = Guice.createInjector(new AppConfiguration());
 		Scanner scanner = new Scanner(System.in);
 		String line = "";
 		ConsoleEventParser parser = injector.getInstance(ConsoleEventParser.class);
+		LOG.info("}]]] " + parser.toString());
 
 		while (!(line = scanner.nextLine()).isEmpty()) {
 			EducationAction action;
@@ -38,11 +41,17 @@ public class App {
 				action = parser.parse(line);
 				action.perform();
 			} catch (EducationException e) {
-				LOG.log(Level.SEVERE, e.getMessage());
+				LOG.warn(e.getMessage(), e);
 			}
 
 		}
 		scanner.close();
+
+	}
+
+	
+	public String toString() {
+		return "compiled message";
 
 	}
 }
